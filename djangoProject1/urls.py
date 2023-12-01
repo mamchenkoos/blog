53aaf55ceae629b43ea42c27921644e5c8327174
Olga <mam4enko.os@yandex.ru>
2023-12-02 01:35:24 +0300

    Change name

--- djangoProject1/urls.py
+++ djangoProject1/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for djangoProject1 project.
+URL configuration for djangoProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,15 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users import views as user_views
-from django.contrib.auth import views as auth_views
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
-    path('accounts/profile/', user_views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('', include('blog.urls'))
 ]
