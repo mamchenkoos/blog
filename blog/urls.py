69b9ec89028414e5699afe980aece6e9b59a571b
Olga <mam4enko.os@yandex.ru>
2023-12-02 01:25:11 +0300

    Done something

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import BlogListView, AboutPageView, ImputPageView
+from .views import BlogListView, AboutPageView, InputPageView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
-    path('about/', AboutPageView.as_view(), name='about'),
-    path('imput/', ImputPageView.as_view(), name='imput')
+    path('about', AboutPageView.as_view(), name='about'),
+    path('input', InputPageView.as_view(), name='input')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,17 @@
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 from django.views.generic import ListView, TemplateView
 from .models import Post
+from django.shortcuts import render
 
 
-@method_decorator(login_required, name='dispatch')
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
-    paginate_by = 4
 
 
-@method_decorator(login_required, name='dispatch')
 class AboutPageView(TemplateView):
     template_name = 'about.html'
 
 
-@method_decorator(login_required, name='dispatch')
-class ImputPageView(TemplateView):
-    template_name = 'imput.html'
+class InputPageView(TemplateView):
+    template_name = 'input.html'
+
